a624fb66ea94443ee2fe1daa684490cf1eb9cc9e
Pranay Katnawer <pkatnawe@uwo.ca>
2022-10-15 14:30:02 -0400

    browse

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ bootstrap = Bootstrap5(app)
 def index():
     return render_template('index.html')
 
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
